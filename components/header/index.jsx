15b20a4777284e5554162c4b653b29db2abe5ae0
brunors1219 <brunors1219@gamil.com>
2024-07-25 22:35:25 -0300

    enviando projeto

---
+++ components/header/index.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import logo from "/public/img/logo.png";
+import Link from "next/link";
+import Styles from "./header.module.scss";
+
+const Header = () => {
+    return <div className={Styles.conteiner}>
+        <div className={Styles.logo}>
+            <Link href="/"><Image src={logo}/></Link>
+        </div>
+        <div className={Styles.menu}>
+            <Link href="/">Home</Link>
+            <Link href="/">Empresa</Link>
+            <Link href="/">Contato</Link>
+        </div>
+    </div>;
+}
+
+export default Header;
