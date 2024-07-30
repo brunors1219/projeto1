755890c320b892f698e707190762ecfeb5489047
brunors1219 <brunors1219@gamil.com>
2024-07-30 17:31:14 -0300

    Alteração de reposividade

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <Header/>
         <Welcome/>
         <Objective/>
-        <Contato/>
+        {/*<Contato/>*/}
         {/*<Footer/>*/}
       </div>
   );
