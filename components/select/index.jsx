c1cd370d9e60255c836b33c3858abd33981b34be
brunors1219 <brunors1219@gamil.com>
2024-07-31 23:46:07 -0300

    Alteração de reposividade

--- components/select/index.jsx
+++ components/select/index.jsx
@@ -1,7 +1,7 @@
 import Styles from "./select.module.scss"
 
-const Select = ({placeholder, required, option}) => {
-    return <select className={Styles.select} required= {required}>
+const Select = ({placeholder, required, option, type, name }) => {
+    return <select className={Styles.select} required= {required} name={name} type={type}>
         <option value="">{placeholder}</option>
         {option?.map((option, index)=> (
             <option key={index} value={option.value}>
