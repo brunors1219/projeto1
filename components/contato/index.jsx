a81a47536a51bcce401972c37844e91ab2a00372
brunors1219 <brunors1219@gamil.com>
2024-07-31 23:36:51 -0300

    Alteração de reposividade

--- components/contato/index.jsx
+++ components/contato/index.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+
 import Input from "../input";
 import Select from "../select";
 import Styles from "./contato.module.scss";
@@ -7,11 +7,6 @@ import Styles from "./contato.module.scss";
 //Formulario de contato
 const Contato = () => {
 
-    const SendEmail= () =>{
-        axios.post("/sendEmail").
-        then(()=>cosole.log("Uhuuu"))
-        .catch(()=>VideoColorSpace.log("Opps"));
-    }
     return (
         <div className={Styles.container}>
             <div className={Styles.text}>
@@ -26,12 +21,12 @@ const Contato = () => {
                 <form action="https://formsubmit.co/bww963w@gmail.com" method="POST">
                     <Input type="text" name="nome" placeholder="Nome" required/>
                     <Input type="email" name="email" placeholder="Email"  required/>
-                    <Input type="text"
+                    <Input type="text" name= "Numero Whatsapp"
                     placeholder="Numero Whatsapp"  
-                    pattern="^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$"
+                    pattern="\(\d{2}\) \d{1-5}-\d{4}"
                     required/>
 
-                    <Select placeholder="Escolha tipo de invenstimento" option=
+                    <Select name= "Seleciona investimento" placeholder="Escolha tipo de invenstimento" option=
                     {[
                     {label: "Ações", valeu: "ações"},
                     {label: "ETF", valeu: "ETFs"},
